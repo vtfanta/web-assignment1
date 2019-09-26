57d2dca9387f81ce6b8eb61173576666669649fa
fantavit <fantavit@fel.cvut.cz>
2019-09-26 14:47:27 +0200

    fixed register route

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
-// app.use('/register', registerRouter);
+app.use('/register', registerRouter);
 
 
 // catch 404 and forward to error handler
