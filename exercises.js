e5835747022c9369b5479631574f2a3ce4aeee24
Hasardine <39343981+Hasardine@users.noreply.github.com>
2019-09-27 10:31:11 +0200

    a

--- exercises.js
+++ exercises.js
@@ -1,85 +1,85 @@
 var exerciseList = [{
 		name: "Lunges",
-		description: '1. Start by standing with your feet shoulder-width apart and arms down at your sides. \n2. Take a step forward with your right leg and bend your right knee as you do so, stopping when your thigh is parallel to the ground. Ensure that your right knee doesn’t extend past your right foot.\n3. Push up off your right foot and return to the starting position. Repeat with your left leg. This is one rep.',
+		description: '1. Start by standing with your feet shoulder-width apart and arms down at your sides. \n2. Take a step forward with your right leg and bend your right knee as you do so, \n \t stopping when your thigh is parallel to the ground.  \n \t Ensure that your right knee doesn’t extend past your right foot.\n3. Push up off your right foot and return to the starting position.\n \t Repeat with your left leg. This is one rep.',
 		numSets: 3,
 		numReps: 15
 	},
 		    {
 			name: 'Squats',
-			description: '1.Start by standing straight, with your feet slightly wider than shoulder-width apart, and your arms at your sides. \n2. Brace your core and, keeping your chest and chin up, push your hips back and bend your knees as if you’re going to sit in a chair. \n3. Ensuring your knees don’t bow inward or outward, drop down until your thighs are parallel to the ground, bringing your arms out in front of you in a comfortable position. Pause for one second, then extend your legs and return to the starting position.',
+			description: '1.Start by standing straight, with your feet \n \t slightly wider than shoulder-width apart, and your arms at your sides. \n2. Brace your core and, keeping your chest and chin up, \n \t push your hips back and bend your knees as if you’re going to sit in a chair. \n3. Ensuring your knees don’t bow inward or outward,  \n \t  drop down until your thighs are parallel to the ground,  \n \t  bringing your arms out in front of you in a comfortable position.  \n \t Pause for one second, then extend your legs and return to the starting position.',
 			numSets: 3,
 			numReps: 20
 
 		    },
 		    {
 			name: 'Jump squats',
-			description: '1. Stand with your body facing forward and your feet parallel, directly underneath your shoulders. \n2. Move your feet a few inches apart with your toes pointed slightly outward. \n3. Lower yourself into the squat, lowering your hips back and down while bending your knees. \n4. Keep your chest upright, with your head and face forward. \n5. Get into as deep a squat as possible, and then explode forcefully upward into a jump.',
+			description: '1. Stand with your body facing forward and your feet parallel, \n \t  directly underneath your shoulders. \n2. Move your feet a few inches apart with your toes pointed slightly outward. \n3. Lower yourself into the squat,  \n \t lowering your hips back and down while bending your knees. \n4. Keep your chest upright, with your head and face forward. \n5. Get into as deep a squat as possible,  \n \t and then explode forcefully upward into a jump.',
 			numSets: 3,
 			numReps: 20
 			},
 		    {
 			name: 'Burpees',
-			description: '1. Start by standing upright with your feet shoulder-width apart and your arms down at your sides. \n2. With your hands out in front of you, start to squat down. When your hands reach the ground, pop your legs straight back into a pushup position. \n3. Do a pushup. \n4. Come back up to the starting pushup position and jump your feet up to your palms by hinging at the waist. Get your feet as close to your hands as you can get, landing them outside your hands if necessary. \n5. Stand up straight, bringing your arms above your head and jump.',
+			description: '1. Start by standing upright with your feet shoulder-width  \n \t  apart and your arms down at your sides. \n2. With your hands out in front of you, start to squat down.  \n \t When your hands reach the ground, \n \t  pop your legs straight back into a pushup position. \n3. Do a pushup. \n4. Come back up to the starting pushup position and jump your \n \t  feet up to your palms by hinging at the waist. \n \t  Get your feet as close to your hands as you can get, \n \t  landing them outside your hands if necessary. \n5. Stand up straight, bringing your arms above your head and jump.',
 			numSets: 3,
 			numReps: 10
 		    },
 		    {
 			name: 'Situps',
-			description: '1. Start by lying on the ground on your back with your knees bent, feet flat, and your hands behind your head. \n2. Keeping your feet glued to the ground, begin to roll up from your head, engaging your core throughout. Don’t strain your neck during the upward motion. \n3. When your chest reaches your legs, begin the controlled phase back down to the starting position.',
+			description: '1. Start by lying on the ground on your back with your knees bent, feet flat, \n \t  and your hands behind your head. \n2. Keeping your feet glued to the ground,  \n \t begin to roll up from your head, engaging your core throughout. \n \t  Don’t strain your neck during the upward motion. \n3. When your chest reaches your legs, begin the controlled  \n \t phase back down to the starting position.',
 			numSets: 3,
 			numReps: 15
 		    },
 		    {
 			name: 'Glute bridge',
-			description: '1. Start by lying on the floor with your knees bent, feet flat on the ground, and arms straight at your sides with your palms facing down. \n2. Pushing through your heels, raise your hips off the ground by squeezing your core, glutes, and hamstrings. Your upper back and shoulders should still be in contact with the ground, and your core down to your knees should form a straight line. \n3. Pause 1-2 seconds at the top and return to the starting position.',
+			description: '1. Start by lying on the floor with your knees bent, \n \t  feet flat on the ground, and arms straight \n \t  at your sides with your palms facing down. \n2. Pushing through your heels, \n \t  raise your hips off the ground by squeezing your core,  \n \t glutes, and hamstrings. Your upper back and shoulders  \n \t should still be in contact with the ground,  \n \t and your core down to your knees should form a straight line. \n3. Pause 1-2 seconds at the top and return to the starting position.',
 			numSets: 3,
 			numReps: 15
 		    },
 		    {
 			name: 'Plank',
-			description: '1. To start, lie flat on your stomach (hips touching the ground) with your legs flat and upper body propped up by your forearms. \n2. Tightening your lower back and shoulder muscles, raise your hips off the ground. \n3. Hold for as long as you can.',
+			description: '1. To start, lie flat on your stomach (hips touching the ground) \n \t  with your legs flat and upper body propped up by your forearms. \n2. Tightening your lower back and shoulder muscles, raise your hips off the ground. \n3. Hold for as long as you can.',
 			numSets: 1,
 			numReps: 40
 		    },
 			{
 			name: 'Slide planks',
-			description: '1. Lie on your right side with your left leg and foot stacked on top of your right leg and foot. Prop your upper body up by placing your right forearm on the ground, elbow directly under your shoulder. \n2. Contract your core to stiffen your spine and lift your hips and knees off the ground, forming a straight line with your body. \n3. Return to start in a controlled manner.',
+			description: '1. Lie on your right side with your left leg and foot stacked on top of your right leg and foot.  \n \t Prop your upper body up by placing your right forearm on the ground,  \n \t elbow directly under your shoulder. \n2. Contract your core to stiffen your spine and lift your hips and knees off the ground, \n \t  forming a straight line with your body. \n3. Return to start in a controlled manner.',
 			numSets: 3,
 			numReps: 15
 		    },
 		    {
 			name: 'Standard pushups',
-			description: '1. Start in a plank position with your pelvis tucked in, your neck neutral, and your palms directly under your shoulders. Make sure your shoulders are rotated back and down, too. \n2. As you brace your core and keep your back flat, begin to lower your body by bending your elbows while keeping them pointed slightly back. Lower down until your chest grazes the floor. \n3. Immediately extend your elbows and push your body back up to the starting position.',
+			description: '1. Start in a plank position with your pelvis tucked in, your neck neutral,  \n \t and your palms directly under your shoulders. Make sure your shoulders are rotated back and down, too. \n2. As you brace your core and keep your back flat, \n \t  begin to lower your body by bending your elbows while keeping them pointed slightly back. \n \t  Lower down until your chest grazes the floor. \n3. Immediately extend your elbows and push your body back up to the starting position.',
 			numSets: 3,
 			numReps: 10
 		    },
 		    {
 			name: 'Modified pushups',
-			description: '1. Start on all fours, keeping a neutral neck. \n2. Walk your hands out until your torso is straight behind you, and your body forms a straight line between the shoulders and the knees. Make sure your shoulders are rotated back and down, and your wrists are stacked directly below your shoulders. Arms should be straight. \n3. Keeping your elbows pointed slightly back, bend at your elbows and lower your whole body down until your upper arms are parallel to the ground. Keep your core tight during this movement. \n4. Once you reach parallel, push up through your palms, extending your elbows and returning to the initial position in step 2.',
+			description: '1. Start on all fours, keeping a neutral neck. \n2. Walk your hands out until your torso is straight behind you, \n \t  and your body forms a straight line between the shoulders and the knees. \n \t  Make sure your shoulders are rotated back and down, \n \t  and your wrists are stacked directly below your shoulders. Arms should be straight. \n3. Keeping your elbows pointed slightly back,  \n \t bend at your elbows and lower your whole body down until your upper arms are parallel to the ground. \n \t  Keep your core tight during this movement. \n4. Once you reach parallel, push up through your palms, \n \t  extending your elbows and returning to the initial position in step 2.',
 			numSets: 3,
 			numReps: 10
 		    },
 		    {
 			name: 'Wide pushups',
-			description: '1. Start in a plank position but with your hands out wider than your shoulders. \n2. Begin to lower your body by bending your elbows, keeping your core tight and your back flat, until your chest grazes the floor. Elbows will flare more than in a standard pushup. \n3. Immediately extend your elbows and push your body back up.',
+			description: '1. Start in a plank position but with your hands out wider than your shoulders. \n2. Begin to lower your body by bending your elbows, keeping your core tight and your back flat, \n \t  until your chest grazes the floor. Elbows will flare more than in a standard pushup. \n3. Immediately extend your elbows and push your body back up.',
 			numSets: 3,
 			numReps: 10
 		    },
 		    {
 			name: 'Narrow pushups',
-			description: '1. Start on the floor and place your hands directly under your chest, closer than shoulder-width apart. \n2. Begin lowering your body by bending your elbows, keeping your core tight and your back flat, until your chest grazes the floor. Keep your elbows tucked in toward your body. \n3. Extend your elbows and push your body back up, using your triceps and chest.',
+			description: '1. Start on the floor and place your hands directly under your chest,  \n \t closer than shoulder-width apart. \n2. Begin lowering your body by bending your elbows, keeping your core tight and your back flat, \n \t  until your chest grazes the floor. Keep your elbows tucked in toward your body. \n3. Extend your elbows and push your body back up, using your triceps and chest.',
 			numSets: 3,
 			numReps: 10
 		    },
 		    {
 			name: 'Decline pushups',
-			description: '1. Start in a plank position, with hands stacked under your shoulders. Put your feet up onto a bench or box. \n2. Begin lowering your body by bending your elbows, keeping your core tight and your back flat, until your chest grazes the floor. Keep your elbows pointed slightly back. \n3. Immediately extend your elbows and push your body back up.',
+			description: '1. Start in a plank position, with hands stacked under your shoulders.  \n \t Put your feet up onto a bench or box. \n2. Begin lowering your body by bending your elbows, keeping your core tight and your back flat,  \n \t until your chest grazes the floor. Keep your elbows pointed slightly back. \n3. Immediately extend your elbows and push your body back up.',
 			numSets: 3,
 			numReps: 10
 		    },
 		    {
 			name: 'Plyometric pushups',
-			description: '1. Start in a plank position with your pelvis tucked in, your neck neutral, and your palms directly under your shoulders. \n2. Begin to lower your body by bending your elbows, keeping them pointed slightly back, with your core tight and your back flat, until your chest grazes the floor. \n3. Immediately extend your elbows and push your body back up, but instead of stopping at the top, use force to launch your upper body up through your hands so your palms come off the ground. \n4. Land lightly back on the ground and lower your chest again for another rep. Add a clap at the top for added difficulty.',
+			description: '1. Start in a plank position with your pelvis tucked in, your neck neutral,  \n \t and your palms directly under your shoulders. \n2. Begin to lower your body by bending your elbows, keeping them pointed slightly back,  \n \t with your core tight and your back flat, until your chest grazes the floor. \n3. Immediately extend your elbows and push your body back up,  \n \t but instead of stopping at the top, use force to launch your upper body up  \n \t through your hands so your palms come off the ground. \n4. Land lightly back on the ground and lower your chest again for another rep.  \n \t Add a clap at the top for added difficulty.',
 			numSets: 3,
 			numReps: 10
 		    },
@@ -91,7 +91,7 @@ var exerciseList = [{
 		    },
 		    {
 			name: 'Chinups',
-			description: '1. Stand facing an exercise bar. \n2. Grasp the bar from underneath with your arms in a tight, slightly closer than shoulder-width grip. \n3. Use your biceps to pull you up, bringing your head up over the bar.',
+			description: '1. Stand facing an exercise bar. \n2. Grasp the bar from underneath with your arms in a tight, \n \t  slightly closer than shoulder-width grip. \n3. Use your biceps to pull you up, bringing your head up over the bar.',
 			numSets: 3,
 			numReps: 10
 			}];
